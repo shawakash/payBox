47c4f21e5df3bd3ab0416c39f282a65073fea928
Akash Shaw <shawakash2003@gmail.com>
2024-01-13 12:06:59 +0530

    feat: preparing db

--- packages/common/src/constant.ts
+++ packages/common/src/constant.ts
@@ -1,2 +1,6 @@
 export const PORT: number = 8080;
 export const SolanaRpcUrl = "https://api.devnet.solana.com";
+export const BACKEND_URL = "http://localhost:8080";
+export const HASURA_URL = "http://localhost:8112/v1/graphql"
+export const HASURA_ADMIN_SERCRET = "myadminsecretkey"
+export const REDIS_URL = "redis://localhost:6379";
