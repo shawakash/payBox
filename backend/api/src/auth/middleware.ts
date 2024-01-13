85f7a6da1dcfd214aee75cb70250bb9e1ac34460
Akash Shaw <shawakash2003@gmail.com>
2024-01-13 17:23:18 +0530

    feat: adding middleware

---
+++ backend/api/src/auth/middleware.ts
@@ -0,0 +1,48 @@
+import { NextFunction, Request, Response } from "express";
+import { clearCookie, setJWTCookie, validateJwt } from "./utils";
+import { responseStatus } from "../types/client";
+
+export const extractClientId = async (
+    req: Request, res: Response, next: NextFunction
+) => {
+    let jwt = "";
+
+    // Header takes precedence
+    const authHeader = req.headers["authorization"];
+    if (authHeader && authHeader.split(" ")[0] === "Bearer") {
+      jwt = authHeader.split(" ")[1];
+    } else if (req.cookies.jwt) {
+      jwt = req.cookies.jwt;
+    } else if (req.query.jwt) {
+      jwt = req.query.jwt as string;
+    }
+  
+    if (jwt) {
+      try {
+        const payloadRes = await validateJwt(jwt);
+        if (payloadRes.payload.sub) {
+          // Extend cookie or set it if not set
+          await setJWTCookie(req, res, payloadRes.payload.sub);
+          // Set id on request
+          //@ts-ignore
+          req.id = payloadRes.payload.sub;
+          // Set jwt  on request
+          //@ts-ignore
+          req.jwt = jwt;
+        }
+      } catch {
+        clearCookie(res, "jwt");
+        return res.status(403).json({ msg: "Auth error", status: responseStatus.Error });
+      }
+    } else {
+      return res.status(403).json({ msg: "No authentication token found", status: responseStatus.Error });
+    }
+  
+    next();
+};
+
+export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  //@ts-ignore
+  req.authHeader = req.headers.authorization;
+  next();
+};
---
+++ backend/api/src/auth/utils.ts
@@ -0,0 +1,57 @@
+import type { Request, Response } from "express";
+import { importPKCS8, importSPKI, jwtVerify, SignJWT } from "jose";
+
+import { AUTH_JWT_PRIVATE_KEY, AUTH_JWT_PUBLIC_KEY } from "../config";
+
+export const alg = "RS256";
+
+export const validateJwt = async (jwt: string) => {
+  const publicKey = await importSPKI(AUTH_JWT_PUBLIC_KEY, alg);
+  return await jwtVerify(jwt, publicKey, {
+    issuer: "shawakash",
+    audience: "payBox",
+  });
+};
+
+export const clearCookie = (res: Response, cookieName: string) => {
+  res.clearCookie(cookieName);
+};
+
+export const setJWTCookie = async (
+  req: Request,
+  res: Response,
+  userId: string
+) => {
+  const secret = await importPKCS8(AUTH_JWT_PRIVATE_KEY, alg);
+
+  const jwt = await new SignJWT({
+    sub: userId,
+  })
+    .setProtectedHeader({ alg })
+    .setIssuer("shawakash")
+    .setAudience("payBox")
+    .setIssuedAt()
+    .sign(secret);
+
+  setCookieOnResponse(req, res, "jwt", jwt);
+
+  return jwt;
+};
+
+export const setCookieOnResponse = (
+  req: Request,
+  res: Response,
+  cookieName: string,
+  cookieValue: string
+) => {
+  res.cookie(cookieName, cookieValue, {
+    secure: true,
+    httpOnly: true,
+    sameSite: "strict",
+    // Note: the leading . below is significant, as it enables us to use the
+    // cookie on subdomains
+    domain: req.hostname.includes("localhost") ? "localhost" : ".rentlr.dev",
+    path: "/",
+    maxAge: 60 * 60 * 24 * 365, // approx 1 year
+  });
+};
