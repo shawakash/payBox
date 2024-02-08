c96de3ed2a341bc2dceb7bade0dc7130e4b2e875
Akash Shaw <shawakash2003@gmail.com>
2024-02-08 19:17:49 +0530

    fix: validate-spec for client route

--- openapi/src/spec-client/models/Cluster.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do no edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-export enum Cluster {
-    DEVNET = 'devnet',
-    TESTNET = 'testnet',
-    MAINNET_BETA = 'mainnet-beta',
-}
--- openapi/src/spec-client/models/TxnArray.ts
+++
@@ -1,6 +0,0 @@
-/* generated using openapi-typescript-codegen -- do no edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-import type { TxnType } from './TxnType';
-export type TxnArray = Array<TxnType>;
