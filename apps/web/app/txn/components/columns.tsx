301547b9fdd9c07213c1750f8a27b43f5f561ead
Akash Shaw <shawakash2003@gmail.com>
2024-02-15 19:53:53 +0530

    chore: fixing the postbalance, preBalance and slot being null and changing the url to be dynaminc

--- apps/web/app/txn/components/columns.tsx
+++ apps/web/app/txn/components/columns.tsx
@@ -10,8 +10,9 @@ import { DataTableColumnHeader } from "./data-table-column-header";
 import { DataTableRowActions } from "./data-table-row-actions";
 import { format } from "date-fns";
 import { CalendarIcon, ClockIcon } from "@radix-ui/react-icons";
-import { SOLSCAN_ACCOUNT_URL, SOLSCAN_TXN_URL, TxnType } from "@paybox/common";
+import { SOLSCAN_ACCOUNT_URL, SOLSCAN_TXN_URL, TxnType, getTransactionUrl } from "@paybox/common";
 import Link from "next/link";
+import { get } from "http";
 
 export const columns: ColumnDef<TxnType>[] = [
   {
@@ -48,7 +49,7 @@ export const columns: ColumnDef<TxnType>[] = [
       // make the cluster dynamic
       <Link
         target="_blank"
-        href={SOLSCAN_TXN_URL(row.original.signature[0], "devnet")}
+        href={getTransactionUrl(row.original.network, row.original.signature[0], row.original.cluster)}
       >
         <div className="w-[80px]">
           Txn-{(row.getValue("id") as string).split("-")[1]}
