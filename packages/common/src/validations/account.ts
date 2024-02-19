4e6b2bbdf7ac0d4311fec2cc7a804935bb4bbec2
Akash Shaw <shawakash2003@gmail.com>
2024-02-19 20:47:30 +0530

    chore(common): adding account name update query validation

--- packages/common/src/validations/account.ts
+++ packages/common/src/validations/account.ts
@@ -1,12 +1,28 @@
 import z from "zod";
 
 export const AccountCreateQuery = z.object({
-    name: z.string()
+    name: z
+        .string()
         .regex(
             /^[A-Za-z]+([- ]?[A-Za-z]+)*$/,
             "should be between 3-15 characters and can only contain numbers, letters, and underscores.",
         ),
     walletId: z
+        .string()
+        .regex(
+            /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/,
+            "should be a valid UUID.",
+        ),
+});
+
+export const AccountNameQuery = z.object({
+    name: z
+        .string()
+        .regex(
+            /^[A-Za-z]+([- ]?[A-Za-z]+)*$/,
+            "should be between 3-15 characters and can only contain numbers, letters, and underscores.",
+        ),
+    accountId: z
         .string()
         .regex(
             /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/,
