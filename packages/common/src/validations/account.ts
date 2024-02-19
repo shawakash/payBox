97d054f3247b360ce743e15c464f747b412ef530
Akash Shaw <shawakash2003@gmail.com>
2024-02-19 21:21:16 +0530

    chore(common): adding AccountGetPrivateKey validation

--- packages/common/src/validations/account.ts
+++ packages/common/src/validations/account.ts
@@ -1,4 +1,5 @@
 import z from "zod";
+import { Network } from "../types";
 
 export const AccountCreateQuery = z.object({
     name: z
@@ -28,4 +29,20 @@ export const AccountNameQuery = z.object({
             /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/,
             "should be a valid UUID.",
         ),
+});
+
+export const AccountGetPrivateKey = z.object({
+    password: z
+        .string()
+        .regex(
+            /^[a-z0-9_]{3,50}$/,
+            "should be between 3-15 characters and can only contain numbers, letters, and underscores.",
+        ),
+    accountId: z
+        .string()
+        .regex(
+            /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/,
+            "should be a valid UUID.",
+        ),
+    network: z.nativeEnum(Network),
 });
