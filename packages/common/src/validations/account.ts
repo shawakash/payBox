36c853c817feff8ae96ac14e63865192a0cc326a
Akash Shaw <shawakash2003@gmail.com>
2024-02-19 20:17:40 +0530

    chore(common): adding query validation for account / post

---
+++ packages/common/src/validations/account.ts
@@ -0,0 +1,15 @@
+import z from "zod";
+
+export const AccountCreateQuery = z.object({
+    name: z.string()
+        .regex(
+            /^[A-Za-z]+([- ]?[A-Za-z]+)*$/,
+            "should be between 3-15 characters and can only contain numbers, letters, and underscores.",
+        ),
+    walletId: z
+        .string()
+        .regex(
+            /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/,
+            "should be a valid UUID.",
+        ),
+});
--- packages/common/src/validations/index.ts
+++ packages/common/src/validations/index.ts
@@ -1,3 +1,4 @@
 export * from "./clientValid";
 export * from "./addressValid";
 export * from "./transaction";
+export * from "./account";
