94226872c18c8a4ba2a9890c26ace349f606ff7b
Akash Shaw <shawakash2003@gmail.com>
2024-02-28 17:07:02 +0530

    chore(api): adding / get qrcode based on account endpoint

--- backend/api/src/routes/qrcode.ts
+++ backend/api/src/routes/qrcode.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { createReadStream } from "fs";
-import { Address, responseStatus } from "@paybox/common";
+import { AccountType, Address, QrcodeQuery, responseStatus } from "@paybox/common";
 import { generateQRCode } from "../auth/util";
-import { hasAddress } from "../auth/middleware";
+import { checkValidation, hasAddress, isValidated } from "../auth/middleware";
+import { cache } from "..";
 
 export const qrcodeRouter = Router();
 
@@ -12,7 +13,7 @@ qrcodeRouter.get("/get", hasAddress, async (req, res) => {
     const address = req.address;
     if (address) {
       const isGenerated = await generateQRCode(
-        address as Partial<Address> & { id: string },
+        address as Partial<Address>,
         address.id,
       );
       if (!isGenerated) {
@@ -26,6 +27,48 @@ qrcodeRouter.get("/get", hasAddress, async (req, res) => {
 
       stream.pipe(res);
     }
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ status: responseStatus.Error, msg: "Internal Server Error" });
+  }
+});
+
+
+qrcodeRouter.get('/', checkValidation, async (req, res) => {
+  try {
+    const { accountId } = QrcodeQuery.parse(req.query);
+
+    //cache
+    const acocunt = await cache.account.getAccount<AccountType>(accountId);
+    if (!acocunt) {
+      return res
+        .status(400)
+        .json({
+          status: responseStatus.Error,
+          msg: "Account not found"
+        });
+      }
+    const code = await generateQRCode(
+      {
+        sol: acocunt.sol.publicKey,
+        eth: acocunt.eth.publicKey,
+        bitcoin: acocunt.bitcoin?.publicKey,
+      },
+      acocunt.id,
+    );
+    if (!code) {
+      return res.status(500).json({
+        status: responseStatus.Error,
+        msg: "Error in generating qr code",
+      });
+    }
+    res.setHeader("Content-Type", "image/png");
+    const stream = createReadStream(code);
+
+    stream.pipe(res);
+
   } catch (error) {
     console.log(error);
     return res
