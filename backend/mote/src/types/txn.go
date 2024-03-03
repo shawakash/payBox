e08f59dec08c1435f79842457f7675acd7a3ed83
Akash Shaw <shawakash2003@gmail.com>
2024-03-03 20:34:19 +0530

    chore: adding txnSignQuery types

---
+++ backend/mote/src/types/txn.go
@@ -0,0 +1,15 @@
+package types
+
+import (
+    "github.com/go-playground/validator/v10"
+)
+
+var Validate = validator.New()
+
+type TxnSign struct {
+    From string `json:"from" validate:"required"`
+	To string `json:"to" validate:"required"`
+	Amount float64 `json:"amount" validate:"required"`
+	Network string `json:"network" validate:"required"`
+	Cluster string `json:"cluster" validate:"required"`
+}
