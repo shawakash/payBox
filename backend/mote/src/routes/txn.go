0da0799d1166b8eef829ad142f97debebd47d01c
Akash Shaw <shawakash2003@gmail.com>
2024-03-04 20:32:25 +0530

    chore: sign handler returing the sol txnhash

--- backend/mote/src/routes/txn.go
+++ backend/mote/src/routes/txn.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
     "mote/src/types"
+    "mote/src/sockets"
 )
 
 
@@ -17,29 +18,25 @@ func SignHandler (w http.ResponseWriter, r *http.Request) {
         }
     }()
     var txnSignQuery types.TxnSign
-    // queryParams := r.URL.Query()
-
-    // from := queryParams.Get("from")
-    // to := queryParams.Get("to")
-    // amount := queryParams.Get("amount")
-    // network := queryParams.Get("network")
-    // cluster := queryParams.Get("cluster")
-
-    // if from == "" || to == "" || amount == "" || network == "" || cluster == "" {
-    //     http.Error(w, "Missing required query parameters", http.StatusBadRequest)
-    //     return
-    // }
 
     if err := json.NewDecoder(r.Body).Decode(&txnSignQuery); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
 
-    fmt.Println("Received request to sign transaction:", txnSignQuery.From)
+    tx, err := sockets.SendSol(txnSignQuery.From, txnSignQuery.To, txnSignQuery.Amount)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    fmt.Println("hash:", tx.TxnHash)
 
     response := map[string]interface{}{
         "status":  "ok",
         "message": "Transaction signed successfully",
+        "hash":     tx.TxnHash,
+        "txn":     tx.Txn,
     }
     jsonResponse, err := json.Marshal(response)
 	if err != nil {
