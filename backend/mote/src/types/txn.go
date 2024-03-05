ceda62486c5bf7bf702b1be87663d921b1f29250
Akash Shaw <shawakash2003@gmail.com>
2024-03-05 19:00:20 +0530

    chore: adding txn type

--- backend/mote/src/types/txn.go
+++ backend/mote/src/types/txn.go
@@ -1,6 +1,8 @@
 package types
 
 import (
+	"time"
+
     "github.com/go-playground/validator/v10"
 )
 
@@ -19,4 +21,18 @@ type TxnGet struct {
 	Hash string `json:"hash" validate:"required"`
 	// Network string `json:"network" validate:"required"`
 	// Cluster string `json:"cluster" validate:"required"`
+}
+
+type Txn struct {
+	Sig string `json:"hash"`
+	Network string `json:"network"`
+	Cluster string `json:"cluster"`
+	Timestamp time.Time `json:"timestamp"`
+	Slot *uint64 `json:"slot"`
+	From string `json:"from"`
+	To string `json:"to"`
+	BlockHash string `json:"blockHash"`
+	Amt float64 `json:"amount"`
+	Fee float64 `json:"fee"`
+	Status string `json:"status"`
 }
