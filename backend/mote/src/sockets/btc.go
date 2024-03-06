63cebb052720d7ddb0575ed5b7e98641140845ce
Akash Shaw <shawakash2003@gmail.com>
2024-03-06 19:36:06 +0530

    chore: adding wait option to wait for txn to sign

--- backend/mote/src/sockets/btc.go
+++ backend/mote/src/sockets/btc.go
@@ -4,14 +4,16 @@ import (
 	"fmt"
 	"log"
 	"os"
-	
+	"time"
+
 	"mote/src/config"
 
+	"github.com/btcsuite/btcd/btcjson"
 	btc "github.com/btcsuite/btcd/btcutil"
 	"github.com/btcsuite/btcd/rpcclient"
 )
 
-func SendBtc(from string, to string, amt float64, wait bool) (string, error) {
+func SendBtc(from string, to string, amt float64, wait bool) (*btcjson.TxRawResult, error) {
 	rpcClient, err := rpcclient.New(&rpcclient.ConnConfig{
 		Host:         config.BTC_RPC_HOST, // Default RPC port for Bitcoin Core
 		User:         config.BTC_RPC_USER,
@@ -39,8 +41,20 @@ func SendBtc(from string, to string, amt float64, wait bool) (string, error) {
 		os.Exit(1)
 	}
 
+	var txn = &btcjson.TxRawResult{
+		Txid: txHash.String(),
+		Time: time.Now().Unix(),
+	}
+
+	if wait {
+		tx, err := rpcClient.GetRawTransactionVerbose(txHash)
+		if err != nil {
+			log.Fatal("Failed to fetch transaction details:", err)
+		}
+		txn = tx
+	}
 
 
 	log.Println("hash: ", txHash)
-	return txHash.String(), nil
+	return txn, nil
 }
