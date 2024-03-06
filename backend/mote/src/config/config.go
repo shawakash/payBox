018f493e89c9365f676a4b48fed313390d4e167f
Akash Shaw <shawakash2003@gmail.com>
2024-03-06 19:38:17 +0530

    chore adding btc envs

--- backend/mote/src/config/config.go
+++ backend/mote/src/config/config.go
@@ -13,6 +13,11 @@ var (
 	REDIS_SECRET = ""
 	REDIS_URL = "redis://localhost:6379"
     INFURA_SEPOLIA_URL = ""
+    BTC_RPC_USER = "user"
+    BTC_RPC_PASS = "pass"
+    BTC_RPC_HOST = "localhost:8332"
+    BTC_RPC_PORT = "8332"
+    BTC_POST_MODE = true
 )
 
 const (
@@ -42,5 +47,17 @@ func init() {
     }
     if ENV_REDIS_URL := os.Getenv("REDIS_URL"); ENV_REDIS_URL != "" {
         REDIS_URL = ENV_REDIS_URL
+    }
+    if ENV_BTC_RPC_USER := os.Getenv("BTC_RPC_USER"); ENV_BTC_RPC_USER != "" {
+        BTC_RPC_USER = ENV_BTC_RPC_USER
+    }
+    if ENV_BTC_RPC_PASS := os.Getenv("BTC_RPC_PASS"); ENV_BTC_RPC_PASS != "" {
+        BTC_RPC_PASS = ENV_BTC_RPC_PASS
+    }
+    if ENV_BTC_RPC_HOST := os.Getenv("BTC_RPC_HOST"); ENV_BTC_RPC_HOST != "" {
+        BTC_RPC_HOST = ENV_BTC_RPC_HOST
+    }
+    if ENV_BTC_RPC_PORT := os.Getenv("BTC_RPC_PORT"); ENV_BTC_RPC_PORT != "" {
+        BTC_RPC_PORT = ENV_BTC_RPC_PORT
     }
 }
