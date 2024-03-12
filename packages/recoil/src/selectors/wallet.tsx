108a30e10cf8ebb04a9b978b5147167275e3255c
Akash Shaw <shawakash2003@gmail.com>
2024-03-12 21:10:37 +0530

    chore: adding get wallet selector

--- packages/recoil/src/selectors/wallet.tsx
+++ packages/recoil/src/selectors/wallet.tsx
@@ -2,19 +2,23 @@ import { BACKEND_URL } from "@paybox/common";
 import { selector } from "recoil";
 import { walletsAtom } from "../../dist";
 
-export const getWallets = selector({
-    key: "getWallets",
-    get: (jwt) => async ({ }) => {
-        const response = await fetch(`${BACKEND_URL}/wallets`, {
-            method: "GET",
-            headers: {
-                "Content-type": "application/json",
-                "Authorization": `Bearer ${jwt}`,
-            },
-        }).then(res => res.json());
-        return response.wallets;
+export const getWalletsSelector = selector({
+    key: "getWalletsSelector",
+    //@ts-ignore
+    get: async ({get}, jwt: string) => {
+        try {
+            const response = await fetch(`${BACKEND_URL}/wallets`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${jwt}`,
+                },
+            }).then((res) => res.json());
+            return response.wallets;
+            
+        } catch (error) {
+            console.log(error);
+            return error;
+        }
     },
-    // set: ({ set }, newValue) => {
-    //     set(walletsAtom, newValue);
-    // }
 });
