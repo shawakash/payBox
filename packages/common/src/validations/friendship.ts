78d28f0b51da8dd1f7ba1a3cbef3f19df6efe1c7
Akash Shaw <shawakash2003@gmail.com>
2024-03-16 20:29:28 +0530

    chore: typed for friendship table

---
+++ packages/common/src/validations/friendship.ts
@@ -0,0 +1,16 @@
+import {z} from "zod";
+
+export const RequestFriendshipValid = z.object({
+    clientId1: z
+        .string()
+        .regex(
+            /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/,
+            "should be a valid UUID.",
+        ),
+    clientId2: z
+        .string()
+        .regex(
+            /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/,
+            "should be a valid UUID.",
+        ),
+});
