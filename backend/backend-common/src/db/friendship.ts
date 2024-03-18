009cc331ebd9671bdfeb54948a8b0a9a74db1e01
Akash Shaw <shawakash2003@gmail.com>
2024-03-18 20:19:36 +0530

    chore: adding checkFriendship db query function

--- backend/backend-common/src/db/friendship.ts
+++ backend/backend-common/src/db/friendship.ts
@@ -1,6 +1,6 @@
-import {Chain} from "@paybox/zeus";
-import {dbResStatus, HASURA_ADMIN_SERCRET, HASURA_URL, JWT} from "@paybox/common";
-import {FriendshipStatus} from "@paybox/common/src";
+import { Chain } from "@paybox/zeus";
+import { dbResStatus, HASURA_ADMIN_SERCRET, HASURA_URL, JWT } from "@paybox/common";
+import { FriendshipStatus } from "@paybox/common";
 
 const chain = Chain(HASURA_URL, {
     headers: {
@@ -29,12 +29,54 @@ export const requestFriendship = async (
             id: true,
             status: true
         }]
-    }, {operationName: "requestFriendship"});
-    if(response.insert_friendship_one?.id) {
+    }, { operationName: "requestFriendship" });
+    if (response.insert_friendship_one?.id) {
         return {
             id: response.insert_friendship_one.id as string,
             status: dbResStatus.Ok,
             friendshipStatus: response.insert_friendship_one.status as FriendshipStatus
+        }
+    }
+    return {
+        status: dbResStatus.Error
+    }
+}
+
+/**
+ * 
+ * @param id string
+ * @param clientId string
+ * @returns 
+ */
+export const checkFriendship = async (
+    id: string,
+    clientId: string,
+): Promise<{
+    status: dbResStatus,
+    friendshipStatus?: FriendshipStatus
+}> => {
+    const response = await chain("query")({
+        friendship: [{
+            where: {
+                id: { _eq: id },
+                _or: [
+                    { clientId1: { _eq: clientId } },
+                    { clientId2: { _eq: clientId } }
+                ],
+            }
+        }, {
+            status: true
+        }]
+    }, { operationName: "checkFriendship" });
+    if(response.friendship.length === 0) {
+        return {
+            status: dbResStatus.Ok,
+        }
+    }
+    if(response.friendship[0].status) {
+        return {
+            status: dbResStatus.Ok,
+            friendshipStatus: response.friendship[0].status as FriendshipStatus
         }
     }
     return {
