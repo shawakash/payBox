f318ffaebf7f14b42304f74ae8d437afb15ff081
Akash Shaw <shawakash2003@gmail.com>
2024-03-20 20:17:37 +0530

    chore: chat page components

---
+++ apps/web/app/chat/components/chat/chat.tsx
@@ -0,0 +1,33 @@
+import { Message, UserData } from "@/app/chat/data";
+import ChatTopbar from "./chat-topbar";
+import { ChatList } from "./chat-list";
+import React from "react";
+
+interface ChatProps {
+  messages?: Message[];
+  selectedUser: UserData;
+  isMobile: boolean;
+}
+
+export function Chat({ messages, selectedUser, isMobile }: ChatProps) {
+  const [messagesState, setMessages] = React.useState<Message[]>(
+    messages ?? []
+  );
+
+  const sendMessage = (newMessage: Message) => {
+    setMessages([...messagesState, newMessage]);
+  };
+
+  return (
+    <div className="flex flex-col justify-between w-full h-full">
+      <ChatTopbar selectedUser={selectedUser} />
+
+      <ChatList
+        messages={messagesState}
+        selectedUser={selectedUser}
+        sendMessage={sendMessage}
+        isMobile={isMobile}
+      />
+    </div>
+  );
+}
