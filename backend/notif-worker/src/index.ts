dcdd6e450f537fed383fa25a0028b05730f56c06
Akash Shaw <shawakash2003@gmail.com>
2024-03-22 20:12:31 +0530

    chore: connecting to admin in master core

--- backend/notif-worker/src/index.ts
+++ backend/notif-worker/src/index.ts
@@ -2,8 +2,17 @@ import cluster from "cluster";
 import express from "express";
 import os from "os";
 
-import { PORT } from "./config";
+import { KAFKA_ID, KAFKA_URL, PORT } from "./config";
+import { Kafka } from "kafkajs";
+import {WorkerAdmin} from "./kafka/admin";
+import {ConsumerWorker} from "./kafka/consumer";
+import {ProducerWorker} from "./kafka/producer";
 
+
+export const kafka = new Kafka({
+    clientId: KAFKA_ID,
+    brokers: [KAFKA_URL],
+});
 
 const workers: { [workerPid: string]: any } = {},
     count = os.cpus().length;
@@ -25,16 +34,31 @@ if (cluster.isPrimary) {
         spawn();
     });
     const app = express();
+
+    (async () => {
+        await WorkerAdmin.getInstance().init([
+            { topicName: "notif", partitions: 1 },
+        ]);
+
+        // This can be connected in any service/s
+        await ProducerWorker.getInstance().connectProducer();
+        
+
+    })();
+
     app.listen(PORT, async () => {
         console.log(`Server listening on port: ${PORT}\n`);
     });
 } else {
     (async () => {
+        await ConsumerWorker.getInstance().connectCounsumer(
+            "notif-group",
+            ["notif"],
+            true
+        );
         const x = 0;
         while (x < 1) {
             // Run always
-            //   await processQueue();
-
         }
     })();
 }
