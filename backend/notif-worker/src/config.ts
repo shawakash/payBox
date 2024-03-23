50aba248176ac61eb4174c716c5e580fc70acb83
Akash Shaw <shawakash2003@gmail.com>
2024-03-23 20:24:54 +0530

    chore: configs for vapid

--- backend/notif-worker/src/config.ts
+++ backend/notif-worker/src/config.ts
@@ -1,5 +1,5 @@
 import * as dotenv from "dotenv";
-import { NOTIF_WORKER_PORT, KAFKA_NOTIF_URL, KAFKA_NOTIF_CLIENT_ID } from "@paybox/common";
+import { NOTIF_WORKER_PORT, KAFKA_NOTIF_URL, KAFKA_NOTIF_CLIENT_ID, VAPID_PRIVATE_KEY_DEFAULT, VAPID_PUBLIC_KEY_DEFAULT } from "@paybox/common";
 dotenv.config();
 
 export const PORT =
@@ -12,7 +12,7 @@ export const KAFKA_ID =
     process.env.KAFKA_NOTIF_CLIENT_ID || KAFKA_NOTIF_CLIENT_ID;
 
 export const VAPID_PRIVATE_KEY = 
-    process.env.VAPID_PRIVATE_KEY || "";
+    process.env.VAPID_PRIVATE_KEY || VAPID_PRIVATE_KEY_DEFAULT;
 
 export const VAPID_PUBLIC_KEY = 
-    process.env.VAPID_PUBLIC_KEY || "";
+    process.env.VAPID_PUBLIC_KEY || VAPID_PUBLIC_KEY_DEFAULT;
