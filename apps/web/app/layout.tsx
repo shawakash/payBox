4474cfc2d6669c95b3b1f42f5c5fb6d12a0c9b9e
Akash Shaw <shawakash2003@gmail.com>
2024-03-23 20:28:53 +0530

    chore: using the notif wrapper

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -8,11 +8,12 @@ import SessionProvider from "@/components/ui/session-provider";
 import RecoilRootWrapper from "@paybox/recoil/src/hooks/recoilRootWraper";
 import { authOptions } from "./api/auth/[...nextauth]/util";
 import RootChildLayout from "./RootChildLayout";
+import NotifWrapper from "./NotifWrapper";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
-  display: 'swap', 
+  display: 'swap',
   adjustFontFallback: false
 });
 
@@ -48,9 +49,11 @@ export default async function RootLayout({
           disableTransitionOnChange
         >
           <SessionProvider session={session} refetchInterval={5 * 60}>
-            <RecoilRootWrapper>
-              <RootChildLayout children={children} />
-            </RecoilRootWrapper>
+            <NotifWrapper>
+              <RecoilRootWrapper>
+                <RootChildLayout children={children} />
+              </RecoilRootWrapper>
+            </NotifWrapper>
           </SessionProvider>
         </ThemeProvider>
       </body>
