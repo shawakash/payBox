c0d8d0d8c22d86c6a3529bd2eb23f50cf8e45f5a
Akash Shaw <shawakash2003@gmail.com>
2024-03-24 20:13:38 +0530

    chore: added independent worker in api

--- backend/api/src/routes/transaction.ts
+++ backend/api/src/routes/transaction.ts
@@ -8,17 +8,18 @@ import {
   TxnType,
   TxnsQeury,
   responseStatus,
+  unixToISOString,
 } from "@paybox/common";
 import { Router } from "express";
 import { getAllTxn, getTxnByHash, getTxns, insertTxn } from "@paybox/backend-common";
-import { Redis } from "..";
+import { Redis, calculateGas } from "..";
 import { txnCheckAddress } from "../auth/middleware";
 import { dbResStatus } from "../types/client";
-import { publishEthTxn, publishSolTxn } from "@paybox/kafka";
 import { Cluster } from "@solana/web3.js";
 import { EthOps } from "../sockets/eth";
 import { INFURA_PROJECT_ID } from "../config";
 import { SolOps } from "../sockets/sol";
+import { Worker } from "../workers/txn";
 
 export const txnRouter = Router();
 
@@ -41,13 +42,33 @@ txnRouter.post("/send", txnCheckAddress, async (req, res) => {
         /**
          * Publishing the txn payload for que based system
          */
-        await publishEthTxn(
-          transaction,
-          amount,
-          id,
-          network,
-          cluster as EthCluster,
-        );
+        try {
+          await Worker.getInstance().publishOne({
+            topic: "txn4",
+            message: [
+              {
+                partition: 1,
+                key: transaction.hash,
+                value: JSON.stringify({
+                  hash: transaction.hash,
+                  amount,
+                  time: unixToISOString(Number(transaction.blockNumber)),
+                  fee: calculateGas(transaction.gasLimit, transaction.gasPrice),
+                  id,
+                  from: transaction.from,
+                  to: transaction.to,
+                  blockHash: transaction.blockHash,
+                  chainId: Number(transaction.chainId),
+                  slot: Number(transaction.nonce),
+                  network,
+                  cluster,
+                }),
+              },
+            ],
+          });
+        } catch (error) {
+          console.log('Error in publishing: ', error);
+        }
         return res
           .status(200)
           .json({ status: responseStatus.Ok, signature: transaction });
@@ -69,16 +90,34 @@ txnRouter.post("/send", txnCheckAddress, async (req, res) => {
         /**
          * Publishing the txn payload for que based system
          */
-        await publishSolTxn(
-          transaction,
-          blockTime,
-          meta,
-          slot,
-          amount,
-          id,
-          network,
-          cluster as Cluster,
-        );
+        try {
+          const sender = transaction.message?.accountKeys[0].toBase58();
+          const receiver = transaction.message?.accountKeys[1].toBase58();
+          await Worker.getInstance().publishOne({
+            topic: "txn4",
+            message: [
+              {
+                partition: 0,
+                key: transaction.signatures[0] || "",
+                value: JSON.stringify({
+                  hash: transaction.signatures[0],
+                  amount,
+                  fee: meta.fee,
+                  id,
+                  from: sender,
+                  to: receiver,
+                  blockHash: transaction.message.recentBlockhash,
+                  time: unixToISOString(blockTime),
+                  slot,
+                  network,
+                  cluster
+                }),
+              },
+            ],
+          });
+        } catch (error) {
+          console.log('Error in publishing: ', error);
+        }
         return res
           .status(200)
           .json({ status: responseStatus.Ok, signature: instance });
