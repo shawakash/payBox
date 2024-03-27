6c64d3bad0e2c1049cdf4a5c05e2f65c7667f42e
Akash Shaw <shawakash2003@gmail.com>
2024-03-27 22:15:49 +0530

    chore: added default account number atom and gave the accounts atom type

--- packages/recoil/src/atoms/account.tsx
+++ packages/recoil/src/atoms/account.tsx
@@ -1,3 +1,4 @@
+import { AccountType } from "@paybox/common";
 import { atom } from "recoil";
 
 export const accountAtom = atom({
@@ -5,7 +6,13 @@ export const accountAtom = atom({
     key: "accountAtom",
 });
 
-export const accountsAtom = atom({
+export const accountsAtom = atom<AccountType[]>({
     default: [],
     key: "accountsAtom",
+});
+
+//TODO: set this to the default account number by
+export const defaultAccountNumberAtom = atom<number>({
+    default: 1,
+    key: "defaultAccountNumberAtom",
 });
